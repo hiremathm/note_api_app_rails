c6afbd9923d7d9feebf3bbb0e2e9a7a3a8b44476
Hiremath M <shivasorab@gmail.com>
2021-03-22 20:46:35 +0530

    header changes

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -2,6 +2,8 @@ class JsonWebToken
 	class << self
 		def encode(payload, exp = 24.hours.from_now)
 			payload[:exp] = exp.to_i
+			Rails.logger.info "SECRET_KEY_BASE #{Rails.application.secrets.secret_key_base}"
+
 			JWT.encode(payload, Rails.application.secrets.secret_key_base)
 		end
 
