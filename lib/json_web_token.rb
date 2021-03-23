a5f38bf92bce1540d9b7146489eb6fd028282d1a
Hiremath M <shivasorab@gmail.com>
2021-03-23 09:33:50 +0530

    get collabrators api

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -15,7 +15,7 @@ class JsonWebToken
 				if ENV["RAILS_ENV"] == "development"
 					body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
 				else
-					body = JWT.encode(payload, ENV['SECRET_KEY_BASE'])
+					body = JWT.encode(token, ENV['SECRET_KEY_BASE'])
 				end
 
 				return HashWithIndifferentAccess.new body
